4e9b55236cff4bafc85ba487db0256ee327aaeae
LuisVynicius <luismevy@gmail.com>
2024-08-07 14:50:39 -0300

    Completed Project

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("First parameter");
+            int p01 = scanner.nextInt();
+            System.out.println("Second parameter");
+            int p02 = scanner.nextInt();
+            count(p01, p02);
+        } catch (Exception e) {
+            System.err.println("Error");
+        }
+    }
+    public static void count(int p01, int p02) {
+        if (p01 > p02) {
+            throw new RuntimeException("Error");
+        }
+        for (int i = 1; i <= (p02 - p01); i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
     }
 }
